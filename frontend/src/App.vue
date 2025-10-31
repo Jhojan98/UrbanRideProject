124c0877abcaa1b1fdc97cd2e75fcf5748138801
LordGuafa <samoaljureb@gmail.com>
2025-10-31 16:31:08 -0500

    loggin hecho, faltan los endpoints

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,32 @@
+//Aquí se puede poner la land page
+
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <!-- Botón para cambiar entre tema claro y oscuro -->
+    <ToggleTheme />
   </nav>
   <router-view/>
+  
 </template>
 
+<script setup lang="ts">
+// Importamos el componente que controla el tema
+import ToggleTheme from './components/ToggleTheme.vue';
+</script>
+
 <style lang="scss">
+@import "./styles/forms.scss";
+
+/* Estilos globales para aplicar tema */
+body {
+  background: var(--bg);
+  color: var(--text);
+  margin: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
+import SignupView from '@/views/SignupView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -8,12 +10,14 @@ const routes: Array<RouteRecordRaw> = [
     component: HomeView
   },
   {
-    path: '/about',
+    path: '/login',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: LoginView
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: SignupView
   }
 ]
 
