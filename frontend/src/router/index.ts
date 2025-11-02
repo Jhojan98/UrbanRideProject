2f7092f92e7e8d659fb9f4dcd43c9c705e899709
xsarias <xsariasa@udistrital.edu.co>
2025-11-01 22:12:17 -0500

    formulario basico en vue

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -12,13 +12,16 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/login',
     name: 'about',
-    component: LoginView
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
-  // {
-  //   path: '/signup',
-  //   name: 'signup',
-  //   component: SignupView
-  // }
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/SignupView.vue')
+  }
 ]
 //TODO: Descomentar cuando esté el signup listo
 
