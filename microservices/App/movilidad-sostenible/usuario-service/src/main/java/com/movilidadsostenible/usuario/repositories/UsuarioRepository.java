3e751347aba8b6fc232fd6ef526d05ff21e0c0e8
ElSnatix <santiagoguarguati25@gmail.com>
2025-11-03 12:47:01 -0500

    landing localhost para desplegarla

--- microservices/App/movilidad-sostenible/usuario-service/src/main/java/com/movilidadsostenible/usuario/repositories/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.movilidadsostenible.usuario.repositories;
-
-import com.movilidadsostenible.usuario.models.entity.Usuario;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsuarioRepository extends CrudRepository<Usuario, String> {
-}
