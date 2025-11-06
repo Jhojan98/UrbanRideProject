9405d3a567cee63d235ce5a36a71eb219304f1fa
SEAN SEBASTIAN BOLIVAR CALDERON <sean.bolivar.2002@gmail.com>
2025-11-05 20:23:28 -0500

    5 nov

--- microservices/App/movilidad-sostenible/viaje-service/src/main/java/com/movilidadsostenible/viaje_service/clients/BicicletaClientRest.java
+++ microservices/App/movilidad-sostenible/viaje-service/src/main/java/com/movilidadsostenible/viaje_service/clients/BicicletaClientRest.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "bicis-service", url = "bicis-service:8002")
+@FeignClient(name = "bicis-service", url = "${service.bicis.url}")
 public interface BicicletaClientRest {
 
     @GetMapping("/{id}")
