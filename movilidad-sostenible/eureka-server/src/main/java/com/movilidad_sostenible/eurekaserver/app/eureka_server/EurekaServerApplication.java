990ddd721e8f540c009e098c3840afb0817a493f
SEAN SEBASTIAN BOLIVAR CALDERON <sean.bolivar.2002@gmail.com>
2025-11-06 12:20:15 -0500

    ss

---
+++ movilidad-sostenible/eureka-server/src/main/java/com/movilidad_sostenible/eurekaserver/app/eureka_server/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.movilidad_sostenible.eurekaserver.app.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
