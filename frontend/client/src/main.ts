70e68e64433fc7031aec8196a2eb8d5800d094ed
Samuel Aljure Bernal <samoaljureb@gmail.com>
2025-11-23 14:30:08 -0500

    Ya no mete node_modules y se configura firebase

--- frontend/client/src/main.ts
+++ frontend/client/src/main.ts
@@ -4,6 +4,20 @@ import router from './router'
 import { createPinia } from 'pinia'
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "@/styles/global.scss";
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+}
+
+initializeApp(firebaseConfig)
+
 const pinia = createPinia()
 const app = createApp(App)
 
