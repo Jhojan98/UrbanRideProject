5b49d8c0eb11d24a9fcb83588297f63f822f7b47
sean-bolivar <sean.bolivar.2002@gmail.com>
2025-11-28 19:17:02 -0500

    MANTENIMIENTO bicis-service CORRECT

--- movilidad-sostenible/bicis-service/src/main/java/com/movilidadsostenible/bicis_service/services/BicycleServiceImpl.java
+++ movilidad-sostenible/bicis-service/src/main/java/com/movilidadsostenible/bicis_service/services/BicycleServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class BicicletaServiceImpl implements BicycleService {
+public class BicycleServiceImpl implements BicycleService {
 
     @Autowired
     private BicycleRepository repository;
