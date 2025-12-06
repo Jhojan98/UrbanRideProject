03ba2b44a9ee985e8632891a47c420bd491b9fd9
SEAN SEBASTIAN BOLIVAR CALDERON <168286107+SeanBol222@users.noreply.github.com>
2025-12-06 12:15:31 -0500

    AVANCES estaciones-service

--- movilidad-sostenible/estaciones-service/src/main/java/com/movilidadsostenible/estaciones_service/repositories/StationsRepository.java
+++ movilidad-sostenible/estaciones-service/src/main/java/com/movilidadsostenible/estaciones_service/repositories/StationsRepository.java
@@ -4,7 +4,9 @@ import com.movilidadsostenible.estaciones_service.model.entity.Station;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StationsRepository extends JpaRepository<Station, Integer> {
+    List<Station> findByIdCity(Integer idCity);
 }
-
